1879822f96947aa43ed92f89230a1b645726e4db
WI-Hyun-joong <wjh4877@naver.com>
2018-01-30 17:11:17 +0900

    버그 수정

--- addFeature/addFeature.js
+++ addFeature/addFeature.js
@@ -1,21 +1,21 @@
 function createPoint() {
     //[[X0, Y0], [X1, Y1], [X2, Y2]...]
     var inputPath = [
-                     [36.1358724, 130.8750216], [36.3858724, 130.8750216],
-                     [36.3858724, 131.1250216], [36.1358724, 131.1250216]
+                     [35.1358724, 127.8750216], [35.3858724, 127.8750216],
+                     [35.3858724, 129.1250216], [35.1358724, 129.1250216]
                     ]; //도형이 그려질 좌표
 
     var drawLinePathPoint = []; // "EPSG:3857"형식의 좌표를 "EPSG:4326"로 바꾼 후 저장할 배열
 
-    for (i = 0; i < inputPath.length; i++) {
-        drawLinePathPoint.push(ol.proj.transform([Number(linePath[i]), Number(linePath[i])],
+    for (let item of inputPath) {
+        drawLinePathPoint.push(ol.proj.transform([Number(item[1]), Number(item[0])],
             "EPSG:4326", "EPSG:3857"));
     }
 
     var vectorSource = new ol.source.Vector();                          //벡터소스 객체 생성
     var vectorLayer = new ol.layer.Vector({ source: vectorSource });    //벡터레이어 객체 생성
 
-    draw(vectorSource, testLine);
+    draw(drawLinePathPoint, vectorSource, '#3399CC', '#ffffff');
     vmap.addLayer(vectorLayer);
 }
 
